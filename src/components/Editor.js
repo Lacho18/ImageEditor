f2f8ea624400395f86488885e8fd6341763d6701
LachezarGenov <121165891+Lacho18@users.noreply.github.com>
2024-06-24 01:38:39 +0300

    commit

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,7 +1,8 @@
 import "../styles/Editor.css";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Editor() {
+    //Describes the image styles
     const [styleSettings, setStyleSettings] = useState({
         type: "Brightness",
         currentValue: 100,
@@ -13,6 +14,21 @@ export default function Editor() {
         }
     });
 
+    const [imgSrc, setImgSrc] = useState({
+        src: "",
+        scale: 0
+    });
+
+    /*const [imgDirection, setImgDirection] = useState(1);
+
+    const imgStyle = useRef({
+        brightness: 100,
+        saturate: 100,
+        contrast: 100,
+        grayscale: 0
+    });*/
+
+    //called every time the range input has changed
     function getRangeData(event) {
         console.log(event.target.value);
         setStyleSettings(oldStyle => {
@@ -20,13 +36,15 @@ export default function Editor() {
         })
     }
 
+    //Called after a style has changed, in order to set the object data
     function setStyleType(type) {
         let range = getCorrectRange(type);
         setStyleSettings(oldStyle => {
-            return { ...oldStyle, type: type, currentValue: 0, functionality: type !== "Saturation" ? type.toLowerCase() : "saturate", range: range };
+            return { ...oldStyle, type: type, currentValue: range.begin, functionality: type !== "Saturation" ? type.toLowerCase() : "saturate", range: range };
         })
     }
 
+    //Function that sets the border for the styles
     function getCorrectRange(type) {
         let response = {
             min: 0,
@@ -48,7 +66,46 @@ export default function Editor() {
         return response;
     }
 
-    console.log(styleSettings);
+    //Function that sets the image that will be visualized by reading the selected file and setting the img source state
+    function getImgFile(event) {
+        const file = event.target.files[0];
+
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                const img = e.target.result;
+                const myImg = new Image();
+                myImg.onload = () => {
+                    console.log("HEREEEEE");
+                    console.log(myImg.width);
+                    console.log(myImg.height);
+
+                    let imageType = 0;
+                    if (myImg.width > myImg.height) {
+                        imageType = 1
+                    }
+                    else {
+                        imageType = 0;
+                    }
+
+                    setImgSrc(oldData => {
+                        return { src: img, imageType: imageType }
+                    });
+                }
+                myImg.src = e.target.result;
+            };
+            reader.readAsDataURL(file);
+        }
+    }
+
+    function downLoadImg() {
+        let link = document.createElement("a");
+        link.href = imgSrc.src;
+        link.download = "edited_image.png"
+        link.click();
+    }
+
+    //console.log(styleSettings);
 
     return (
         <div className="main-div">
@@ -68,7 +125,7 @@ export default function Editor() {
                             <p style={{ flexBasis: "80%" }}>{styleSettings.type}</p>
                             <p style={{ flexBasis: "20%", textAlign: "right" }}>{styleSettings.currentValue}%</p>
                         </div>
-                        <input type="range" onChange={getRangeData} min={styleSettings.range.min} max={styleSettings.range.max} value={styleSettings.range.begin} />
+                        <input type="range" step={1} onChange={getRangeData} min={styleSettings.range.min} max={styleSettings.range.max} value={styleSettings.currentValue} />
                     </div>
                     <div className="rotation-buttons" style={{ flexBasis: "20%" }}>
                         <button>➔</button>
@@ -78,8 +135,12 @@ export default function Editor() {
                     </div>
                 </div>
                 <div className="image-visual">
-                    <img src="https://dotesports.com/wp-content/uploads/2020/05/18112329/Cosmic-Dust-Xayah.jpg"
-                        style={{ filter: `${styleSettings.functionality}(${styleSettings.currentValue}%)` }}
+                    <img src={imgSrc.src}
+                        style={{
+                            filter: `${styleSettings.functionality}(${styleSettings.currentValue}%)`,
+                            width: imgSrc.imageType === 1 ? "500px" : "300px",
+                            height: imgSrc.imageType === 0 ? "400px" : "300px",
+                        }}
                     />
                 </div>
             </div>
@@ -88,10 +149,12 @@ export default function Editor() {
                     <button>Reset filter</button>
                 </div>
                 <div style={{ justifyContent: "flex-end" }}>
-                    <input type="file" placeholder="Choose image" />
-                    <button>Save image</button>
+                    <input type="file" placeholder="Choose image" accept="image/*" onChange={getImgFile} />
+                    <button onClick={downLoadImg}>Save image</button>
                 </div>
             </div>
         </div>
     )
 }
+
+//src="https://dotesports.com/wp-content/uploads/2020/05/18112329/Cosmic-Dust-Xayah.jpg"
